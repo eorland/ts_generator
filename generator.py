9b7c19004705d6dc676cd8482d4491f17914a03f
Eli Orland <eliorland@Eli-Orlands-MacBook-Pro.local>
2022-07-31 16:50:01 -0400

    initial commit

---
+++ generator.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 31 15:24:49 2022
+
+@author: eliorland
+"""
+
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+import torch
+from torch.utils.data import Dataset, DataLoader
+from transformers import GPT2Tokenizer, GPT2LMHeadModel, AdamW, get_linear_schedule_with_warmup
+from tqdm import tqdm, trange
+import torch.nn.functional as F
+import textwrap as tr
+
+class Generator():
+
+  def __init__(self):
+    
+    self.tokenizer =  GPT2Tokenizer.from_pretrained('gpt2')
+    self.model = torch.load('model.pt')
+
+  def generate_from_prompt(self, prompt, num_attempts=1, entry_length=200, 
+                           top_p=0.8, temperature=1., when_ready=True):
+
+      '''Main generation function. Takes a prompt as input, 
+         and generates an arbitrary number of descriptions 
+         for that prompt. Returns list of unformatted product 
+         descriptions. Can print formatted versions of each 
+         description as soon as it's ready.'''
+
+      self.model.eval()
+      
+      if not when_ready: # extra progress bar info
+        
+        prompt_split = prompt.split('>')
+        product_name = prompt_split[1].split('#')[0]
+        print('Working on:',product_name)
+
+      generated_num = 0
+      generated_output = []
+      filter_value = -float("Inf")
+      disable_cond = when_ready # on/off "switch" for progress bar
+      
+      with torch.no_grad():
+
+        for entry_idx in tqdm(range(num_attempts),disable=disable_cond):
+
+            print('\n')
+
+            entry_finished = False
+            
+            # encode prompt
+            generated = torch.tensor(self.tokenizer.encode(prompt)).unsqueeze(0)
+
+            for i in range(entry_length): # begin writing...
+                
+                outputs = self.model(generated, labels=generated)
+                loss, logits = outputs[:2]
+                logits = logits[:, -1, :] / (temperature if temperature > 0 else 1.0)
+
+                sorted_logits, sorted_indices = torch.sort(logits, descending=True)
+                cumulative_probs = torch.cumsum(F.softmax(sorted_logits, dim=-1), dim=-1)
+
+                sorted_indices_to_remove = cumulative_probs > top_p
+                sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[
+                    ..., :-1
+                ].clone()
+                sorted_indices_to_remove[..., 0] = 0
+
+                indices_to_remove = sorted_indices[sorted_indices_to_remove]
+                logits[:, indices_to_remove] = filter_value
+
+                next_token = torch.multinomial(F.softmax(logits, dim=-1), num_samples=1)
+                
+                generated = torch.cat((generated, next_token), dim=1)
+                
+                if next_token in self.tokenizer.encode("<|endoftext|>"):
+                    entry_finished = True
+                
+                if entry_finished: 
+                    generated_num = generated_num + 1
+                    output_list = list(generated.squeeze().numpy())
+                    output_text = self.tokenizer.decode(output_list)
+                    generated_output.append(output_text)
+                    break
+                
+                
+            if not entry_finished:
+              output_list = list(generated.squeeze().numpy())
+              output_text = f"{self.tokenizer.decode(output_list)}<|endoftext|>" 
+              generated_output.append(output_text)    
+            
+            if when_ready:
+              print(str(entry_idx+1) + '/' + str(num_attempts),end=' - ')
+              self.display_output(output_text)
+            
+      if not when_ready:
+        print('\n')
+      
+      return generated_output
+
+  def write(self,products,entry_length=200,num_attempts=1,when_ready=True):
+      
+      '''Top level function which takes a list of product names,
+         calls the main generation function, and returns the raw model 
+         outputs. Tidy, human readable descriptions are displayed 
+         automatically'''
+         
+      if not isinstance(products,list):
+          products = [products]
+      # to be a nested list - each entry is a list of 
+      # descriptions for each product
+      generated_descriptions = [] 
+      
+      for prompt in products:
+        # format prompt for model input
+        prompt = '<|startoftext|>' + prompt +'\n####\n'
+        
+        # run generator function
+        x = self.generate_from_prompt(prompt, num_attempts=num_attempts,
+                                      entry_length=entry_length,
+                                      when_ready=when_ready)
+        # add all descriptions based on prompt to main list
+        generated_descriptions.append(x)
+        
+      if not when_ready: 
+        for descriptions in generated_descriptions:
+          for description in descriptions:
+            print('\n')
+            self.display_output(description)  
+      
+      return generated_descriptions
+
+  def display_output(self,generated_text):
+      
+      '''Helper function which prints the model outputs in a 
+        clean, human readable format'''
+
+      wrapper = tr.TextWrapper(width=50)    
+      split = generated_text.split('>')
+      product_name = split[1].split("\n####\n")[0]
+      description = split[1].split("\n####\n")[1].replace('<|endoftext|','')
+
+      print(product_name + ':\n')
+
+      word_list = wrapper.wrap(text=description)
+      for element in word_list:
+          print(element)
+
+      print('\n')
+
+      return
+  
+    
---
+++ info_scrape.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  5 11:12:49 2022
+
+@author: eliorland
+"""
+
+import requests
+from bs4 import BeautifulSoup
+import numpy as np
+import pandas as pd
+
+archive_url = 'https://www.taylorstitch.com/collections/mens-archive'
+shirts_url = 'https://www.taylorstitch.com/collections/mens-shirts?sorted=best-selling-sales-count'
+base_url = 'https://www.taylorstitch.com'
+
+url_list = [archive_url, shirts_url]
+
+product_urls = [] # list of all product pages to get later
+
+for url in url_list:
+    site = requests.get(url)
+
+    soup = BeautifulSoup(site.text, 'html.parser')
+
+    products = soup.find_all('ul',{'class':'product matrix'})
+    products = products[0].find_all('a',href=True) # a tags hold products here
+
+    for product in products:
+        product_urls.append(base_url+product['href'])
+        
+# look at each product, pull relevent info.
+# store all info in lists, which will be converted to pandas df later
+product_title = []
+product_description = []
+product_material = []
+
+
+for product in product_urls:
+
+    product_page = requests.get(product)
+    product_soup = BeautifulSoup(product_page.text, 'html.parser')
+    
+    title_info = product_soup.find('h1')['data-title']
+    
+    description_info = product_soup.find_all('div',
+                                             {'id':'collapsible-description'})
+    material_info = product_soup.find_all('div',
+                                             {'id':'collapsible-material'})
+    #if len(description_info)==0:
+    #   continue
+    try: 
+        description = description_info[0].find('p').text
+        material = material_info[0].find('p').text
+    
+    except: 
+        continue
+        
+    product_title.append(title_info)
+    product_description.append(description)
+    product_material.append(material)
+
+    
+all_info = pd.DataFrame(list(zip(product_title, 
+                                 product_description,
+                                 product_material)),
+               columns =['Name', 'Description', 'Material'])
+
+all_info.to_csv('taylor_stitch_info.csv',index=True,header=True)
+
